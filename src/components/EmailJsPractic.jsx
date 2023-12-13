4e22b440084562319b4be39ef1b9a9ce63b38c4e
tarunmalik330 <maliktarun2005@gmail.com>
2023-12-13 14:59:19 +0530

    complete

---
+++ src/components/EmailJsPractic.jsx
@@ -0,0 +1,68 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const EmailJsPractic = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.target.reset(alert("successfull"));
+
+    emailjs
+      .sendForm(
+        "service_nlvjld9",
+        "template_l7jjvqj",
+        form.current,
+        "qxtBsAqACVCOH2sxs"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <>
+      <div className=" d_flex item_center justify_center w_100 pt_100">
+        <form ref={form} onSubmit={sendEmail} className="d_flex flex_column">
+          <div className=" d_flex item_center  gap_15">
+            <label className=" d_flex text_black text_16">
+              Name <span>-</span>
+            </label>
+            <input className=" w_100" type="text" name="user_name" />
+          </div>
+          <div className=" d_flex item_center gap_15 mt_24">
+            <label className=" d_flex">
+              Email <span>-</span>
+            </label>
+            <input className="w_100" type="email" name="user_email" />
+          </div>
+          <div className=" d_flex item_center gap_15 mt_24">
+            <label>
+              {" "}
+              Passward <span>-</span>
+            </label>
+            <input type="password" name="username" />
+          </div>
+          <div className=" d_flex item_center gap_15 mt_24">
+            <label className=" d_flex">
+              Message <span>-</span>
+            </label>
+            <textarea name="message" className="w_100" />
+          </div>
+          <div className="d_flex justify-center text_center item_center">
+            <input
+              className=" w_110 h_50 button mt_32"
+              type="submit"
+              value="Send"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default EmailJsPractic;
